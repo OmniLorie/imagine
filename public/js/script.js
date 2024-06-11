17870ba99ac056b1b5932a706b03c826222e72fb
Loretta Osibe <losibe27@gmail.com>
2024-06-11 15:15:36 +0000

    Arrays in Javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,4 @@
-const btn = document.getElementById('btn');
-btn.onclick = function () {
-   const name = prompt('Enter your full name');
-   document.getElementById('name'). innerText = name;
+condocument.getElementById('name'). innerText = name;
 }
 // variables   
 const pi = 3.142;
@@ -113,4 +110,48 @@ parseInt("3.243")
 parseFloat("3.243")
 let amount = 100000
 console.log(`GHS$(amount)` )
-amount.toString()
+amount.toString()st btn = document.getElementById('btn');
+btn.onclick = function () {
+   const name = prompt('Enter your full name');
+   
+
+
+//write a function that will take a user with firstname, lastname and return a full name;
+
+function fullName(user) {
+  return {
+    ...user,
+    fullName: '${user.firstname} ${user,lastname}'
+   };
+  }
+  
+  const user = {
+  firstname: 'lorie',
+  lastname: 'Osibe'
+  }
+  fullName(user);
+  
+  //Array map 
+  const users = [
+  {firstname: 'Michael', lastname: 'Hammond'},
+  {firstname: 'Elon', lastname: 'Musk'},
+  {firstname: 'Berry', lastname: 'Allen'},
+  {firstname: 'Oliver', lastname: 'Queen'},
+  {firstname: 'Will', lastname: 'Smith'},
+  ]
+  // users.map(fullName);
+  
+  //Square of numbers
+  function square(number) {
+    return number **2;
+  }
+  square(10);
+  const numbers = [9, 8, 7, 6];
+  numbers.map(square);
+  
+  //Array filter
+  function isEven(number) {
+    return number % 2 === 0;
+  }
+  isEven(6);
+  numbers.filter(isEven);
